d8f3a5268fb88fb3b49d58e2d0a830dfdf130355
Kyne2007 <naiping.hz@qq.com>
2020-03-14 00:41:05 +0800

    add context

--- main.go
+++ main.go
@@ -1,21 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"fugui"
 	"net/http"
 )
 
 func main() {
 	r := fugui.New()
-	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+
+	r.GET("/", func(c *fugui.Context) {
+		c.HTML(http.StatusOK, "<h1>hello fugui</h1>")
 	})
 
-	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
-		for k, v := range req.Header {
-			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-		}
+	r.GET("/hello", func(c *fugui.Context) {
+		c.String(http.StatusOK, "hello %s, you are at %s\n", c.Query("name"), c.Path)
+	})
+
+	r.POST("/login", func(c *fugui.Context) {
+		c.JSON(http.StatusOK, fugui.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
 	})
 
 	r.Run(":4444")
