a0a13f33733beeab03bd127c144465feedca8f9c
Kyne2007 <naiping.hz@qq.com>
2020-03-18 00:43:52 +0800

    support group

--- main.go
+++ main.go
@@ -12,19 +12,32 @@ func main() {
 		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 	})
 
-	r.GET("/hello", func(c *fugui.Context) {
-		// expect /hello?name=haha
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *fugui.Context) {
+			c.HTML(http.StatusOK, "<h1>hello fugui</h1>")
+		})
 
-	r.GET("/hello/:name", func(c *fugui.Context) {
-		// expect /hello/haha
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
+		v1.GET("/hello", func(c *fugui.Context) {
+			// expect /hello?name=wuha
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
 
-	r.GET("/assets/*filepath", func(c *fugui.Context) {
-		c.JSON(http.StatusOK, fugui.H{"filepath": c.Param("filepath")})
-	})
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *fugui.Context) {
+			// expect /hello/wuha
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *fugui.Context) {
+			c.JSON(http.StatusOK, fugui.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+
+	}
 
 	r.Run(":4444")
 }
