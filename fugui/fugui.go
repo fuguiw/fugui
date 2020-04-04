82ce69f384d09aa53bcb0ef711442ed34a97c442
Kyne2007 <naiping.hz@qq.com>
2020-04-04 22:22:13 +0800

    support template

--- fugui/fugui.go
+++ fugui/fugui.go
@@ -1,8 +1,10 @@
 package fugui
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path"
 	"strings"
 )
 
@@ -20,8 +22,10 @@ type (
 
 	Engine struct {
 		*RouterGroup
-		router *router
-		groups []*RouterGroup // store all groups
+		router        *router
+		groups        []*RouterGroup // store all groups
+		htmlTemplates *template.Template
+		funcMap       template.FuncMap
 	}
 )
 
@@ -68,6 +72,36 @@ func (group *RouterGroup) Use(middlewares ...HandlerFunc) {
 	group.middlewares = append(group.middlewares, middlewares...)
 }
 
+func (group *RouterGroup) createStaticHandler(relativePath string, fs http.FileSystem) HandlerFunc {
+	absolutePath := path.Join(group.prefix, relativePath)
+	fileServer := http.StripPrefix(absolutePath, http.FileServer(fs))
+	return func(c *Context) {
+		file := c.Param("filepath")
+		log.Println(file)
+		if _, err := fs.Open(file); err != nil {
+			c.Status(http.StatusNotFound)
+			return
+		}
+
+		fileServer.ServeHTTP(c.Writer, c.Req)
+	}
+}
+
+func (group *RouterGroup) Static(relativePath string, root string) {
+	handler := group.createStaticHandler(relativePath, http.Dir(root))
+	urlPattern := path.Join(relativePath, "/*filepath")
+	group.GET(urlPattern, handler)
+}
+
+// for custom render function
+func (engine *Engine) SetFuncMap(funcMap template.FuncMap) {
+	engine.funcMap = funcMap
+}
+
+func (engine *Engine) LoadHTMLGlob(pattern string) {
+	engine.htmlTemplates = template.Must(template.New("").Funcs(engine.funcMap).ParseGlob(pattern))
+}
+
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	var middlewares []HandlerFunc
 	for _, group := range engine.groups {
@@ -78,5 +112,6 @@ func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 
 	c := newContext(w, req)
 	c.handlers = middlewares
+	c.engine = engine
 	engine.router.handle(c)
 }
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"fugui"
+	"html/template"
 	"log"
 	"net/http"
 	"time"
@@ -18,20 +20,40 @@ func onlyForV2() fugui.HandlerFunc {
 	}
 }
 
+type student struct {
+	Name string
+	Age  int8
+}
+
+func formatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
+}
+
 func main() {
 	r := fugui.New()
 	r.Use(fugui.Logger())
 
+	r.SetFuncMap(template.FuncMap{
+		"formatAsDate": formatAsDate,
+	})
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/assets", "./static")
+
+	stu1 := &student{Name: "haha", Age: 20}
+	stu2 := &student{Name: "Jack", Age: 22}
 	r.GET("/", func(c *fugui.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		c.HTML(http.StatusOK, "css.tmpl", nil)
+	})
+	r.GET("/students", func(c *fugui.Context) {
+		c.HTML(http.StatusOK, "arr.tmpl", fugui.H{
+			"title":  "gee",
+			"stuArr": [2]*student{stu1, stu2},
+		})
 	})
 
 	v1 := r.Group("/v1")
 	{
-		v1.GET("/", func(c *fugui.Context) {
-			c.HTML(http.StatusOK, "<h1>hello fugui</h1>")
-		})
-
 		v1.GET("/hello", func(c *fugui.Context) {
 			// expect /hello?name=wuha
 			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
