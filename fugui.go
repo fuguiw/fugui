48fce8fdd4470e06a9ffcc9b9dab1f6bfa3d6560
Kyne2007 <naiping.hz@qq.com>
2021-01-01 22:38:31 +0800

    support recovery

--- fugui.go
+++ fugui.go
@@ -41,6 +41,12 @@ func New() *Engine {
 	return engine
 }
 
+func Default() *Engine {
+	engine := New()
+	engine.Use(Logger(), Recovery())
+	return engine
+}
+
 func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
 	engine.router.addRoute(method, pattern, handler)
 }
